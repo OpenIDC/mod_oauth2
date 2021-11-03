e930babecb24fe6ef0d2cd09de4525edbd4a075d
Brian Spratke <brian.spratke@dematic.com>
2021-11-02 20:46:58 -0400

    bump some versions.

--- setup_and_build.cmd
+++ setup_and_build.cmd
@@ -14,6 +14,6 @@ mkdir target\mod_oauth2\x64
 mkdir target\mod_oauth2\x64\Release
 copy x64\Release\mod_oauth2.dll target\mod_oauth2\x64\Release\mod_oauth2.dll
 
-del target\mod_oauth2-3.2.0-win64-VS16.zip
+del target\mod_oauth2-3.2.2-win64-VS16.zip
 
-powershell Compress-Archive -Path target\mod_oauth2\x64\Release\mod_oauth2.dll -DestinationPath target\mod_oauth2-3.2.0-win64-VS16.zip
+powershell Compress-Archive -Path target\mod_oauth2\x64\Release\mod_oauth2.dll -DestinationPath target\mod_oauth2-3.2.2-win64-VS16.zip
